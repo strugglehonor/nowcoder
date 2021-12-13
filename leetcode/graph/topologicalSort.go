b960e78a1cbe4e1ca3b6ef052c968d30269669c8
strugglehonor <2537731609@qq.com>
2021-12-13 20:52:48 +0800

    refactor

--- leetcode/graph/topologicalSort.go
+++ leetcode/graph/topologicalSort.go
@@ -1,7 +1,7 @@
 package graph
 
 // 拓扑排序，把入度为0的点，打印，然后去除该点及其影响，再找入度为0的点，依次类推
-func (graph Graph) topologicalSort(node *Node) []Node {
+func (graph Graph) topologicalSort() []Node {
 	inMap := map[*Node]int{}
 	zeroQueue := make([]Node, 0)
 	for _, node := range graph.nodes {
@@ -17,6 +17,7 @@ func (graph Graph) topologicalSort(node *Node) []Node {
 		cur := zeroQueue[0]
 		zeroQueue = append(zeroQueue[:0], zeroQueue[1:]...)
 		result = append(result, cur)
+		// 处理所有下一个节点
 		for _, next := range cur.nexts {
 			inMap[&next]--
 			if inMap[&next] == 0 {
